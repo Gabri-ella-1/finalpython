21c674257e3c2e491b5fe95e34b6658b4f160e4b
RileyArunakul <arunakulbiz@gmail.com>
2023-03-27 14:11:58 -0400

    blank input fields

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ function App() {
     <div className="App">
       <form>
         <label>Username:</label>
+        <input></input>
         <br />
         <label>Password:</label>
+        <input></input>
         <br />
         <button type="submit">Log in</button>
       </form>

3ad8c0b467017349649df42261eeea4578d24926
RileyArunakul <arunakulbiz@gmail.com>
2023-03-27 14:10:16 -0400

    Initial frontend commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <label>Username:</label>
+        <br />
+        <label>Password:</label>
+        <br />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
